c8218a465e00da077672daff7162b79a3e1090ba
AngelicaOrtizB <162819623+Ortiz0022@users.noreply.github.com>
2024-06-15 08:26:53 -0600

    Add files via upload

---
+++ ExamenFinal/Service/MyDbContext/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Service.MyDbContext
+{
+    public class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("MyDatabase");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
